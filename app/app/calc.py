6c119c4a10625cced0cc13c39806aa20765c4304
unknown <lex@netholdings.com.au>
2020-03-25 22:39:29 +1100

    Corrected python code to PEP8 standards using flake8

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,4 +1,10 @@
 
-def add(x,y):
-    """Add 2 numbers together"""
+
+def add(x, y):
+    """Add two numbers together"""
     return x + y
+
+
+def subtract(x, y):
+    """subtract x from y numbers"""
+    return y - x
